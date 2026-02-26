e59992105dd353f8d1e26e6df42736e742039353
Dastan <usubakunovdastan58@gmail.com>
2026-02-26 16:51:44 +0600

    добавил код

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 // import { db } from "./firebase";
 // console.log(db);
 import Home from "./pages/Home";
-const handleAddStudent = (student) => {
-  setStudents([...students, student]);
-};
+
 
 function App() {
   const handleAddStudent = (student) => {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,45 +1,5 @@
-import { useState, useEffect } from "react";
-import StudentForm from "../components/StudentForm";
-import StudentList from "../components/StudentList";
-import SearchBar from "../components/SearchBar";
-import FilterBar from "../components/FilterBar";
-import { getStudents } from "../services/studentService";
-
 const Home = () => {
-  const [students, setStudents] = useState([]);
-  const [selectedStudent, setSelectedStudent] = useState(null);
-
-  const fetchStudents = async () => {
-    const data = await getStudents();
-    setStudents(data);
-  };
-
-  useEffect(() => {
-    fetchStudents();
-  }, []);
-
-  return (
-    <div style={{ padding: "40px" }}>
-      <h1>Student Manager</h1>
-
-      <StudentForm
-        selectedStudent={selectedStudent}
-        onFinish={() => {
-          fetchStudents();
-          setSelectedStudent(null);
-        }}
-      />
-
-      <SearchBar />
-      <FilterBar />
-
-      <StudentList
-        students={students}
-        onEdit={setSelectedStudent}
-        onUpdate={fetchStudents}
-      />
-    </div>
-  );
+  return <h1>Student Manager</h1>;
 };
 
 export default Home;
